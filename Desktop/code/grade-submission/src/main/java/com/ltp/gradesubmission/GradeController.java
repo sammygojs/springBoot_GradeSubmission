28efa19caf2e96a80a9c6e6028529cb355f26776
Sumit <akoliyasumit@hotmail.com>
2023-06-26 02:02:32 +0530

    setup

---
+++ Desktop/code/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -0,0 +1,26 @@
+package com.ltp.gradesubmission;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GradeController {
+
+    List<Grade>studentGrades = Arrays.asList(
+        new Grade("sumit", "cs", "100"),
+        new Grade("sumit", "cs", "100"),
+        new Grade("sumit", "cs", "100")
+    );
+
+    @RequestMapping("/")
+    public String index(Model Model){
+        // Model.addAttribute(errormsg, "This is the error msg");
+        // Grade grade = new Grade("sumit", "cs", "100");
+        Model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
+}
