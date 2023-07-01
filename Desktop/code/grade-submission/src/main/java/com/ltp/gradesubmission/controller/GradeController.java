d8f06f99865e2c2adfd91bc794e2efcb222ce2b0
Sumit <akoliyasumit@hotmail.com>
2023-07-01 23:59:08 +0530

    updating GradeService

--- Desktop/code/grade-submission/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ Desktop/code/grade-submission/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ltp.gradesubmission.Constants;
 import com.ltp.gradesubmission.Grade;
-import com.ltp.gradesubmission.repository.GradeRepository;
+import com.ltp.gradesubmission.service.GradeService;
 
 @Controller
 public class GradeController {
 
-    GradeRepository gradeRepository = new GradeRepository();
+    GradeService gradeService = new GradeService();
+    // GradeRepository gradeRepository = new GradeRepository();
 
     @GetMapping("/")
     public String getForm(Model Model, @RequestParam(required = false) String id){
@@ -25,8 +26,8 @@ public class GradeController {
         // return "grades";
 
         // Grade grade;
-        int index = getGradeIndex(id);
-        Model.addAttribute("grade", index==Constants.NOT_FOUND ? new Grade() : gradeRepository.getGrade(index) );
+        // int index = gradeService.getGradeIndex(id);
+        Model.addAttribute("grade", gradeService.getGradeById(id) );
         return "form";
         // if(index==Constants.NOT_FOUND){
         //     grade = new Grade();
@@ -48,30 +49,25 @@ public class GradeController {
                 // return "redirect:/";
                 return "form";
             }
-            int index = getGradeIndex(grade.getId());
-            if(index == Constants.NOT_FOUND)   {
-                // System.out.println("Grade ID not found: " +grade.getId());
-                // studentGrades.add(grade);
-                gradeRepository.addGrade(grade);
-            }else{
-                // studentGrades.set(getGradeIndex(grade.getId()), grade);
-                gradeRepository.updateGrade(index, grade);
-            }
+            gradeService.submitGrade(grade);
+            // int index = gradeService.getGradeIndex(grade.getId());
+            // if(index == Constants.NOT_FOUND)   {
+            //     // System.out.println("Grade ID not found: " +grade.getId());
+            //     // studentGrades.add(grade);
+            //     gradeService.addGrade(grade);
+            // }else{
+            //     // studentGrades.set(getGradeIndex(grade.getId()), grade);
+            //     gradeService.updateGrade(index, grade);
+            // }
             
             return "redirect:/grades";
     }
 
     @GetMapping("/grades")
     public String getGrades(Model model){
-        model.addAttribute("grades", gradeRepository.getGrades());
+        model.addAttribute("grades", gradeService.getGrades());
         return "grades";
     }
 
-    public Integer getGradeIndex(String id){
-        for (int i = 0; i < gradeRepository.getGrades().size(); i++) {
-            System.out.println("current ID: "+gradeRepository.getGrades().get(i).getId());
-            if(gradeRepository.getGrades().get(i).getId().equals(id)) return i;
-        }
-        return Constants.NOT_FOUND;
-    }
+    
 }
--- Desktop/code/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeService.java
+++ Desktop/code/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeService.java
@@ -1,5 +1,52 @@
 package com.ltp.gradesubmission.service;
 
+import java.util.List;
+
+import org.springframework.ui.Model;
+
+import com.ltp.gradesubmission.Constants;
+import com.ltp.gradesubmission.Grade;
+import com.ltp.gradesubmission.repository.GradeRepository;
+
 public class GradeService {
-    
+
+    GradeRepository gradeRepository = new GradeRepository();
+
+     public Grade getGrade(int index){
+        return gradeRepository.getGrade(index);
+    }
+
+    public void addGrade(Grade grade){
+        gradeRepository.addGrade(grade);
+    }
+
+    public void updateGrade(int index, Grade grade){
+        gradeRepository.updateGrade(index, grade);
+    }
+
+    public List<Grade>getGrades(){
+        return gradeRepository.getGrades();
+    }
+
+    public Integer getGradeIndex(String id){
+        for (int i = 0; i < getGrades().size(); i++) {
+            System.out.println("current ID: "+getGrades().get(i).getId());
+            if(getGrades().get(i).getId().equals(id)) return i;
+        }
+        return Constants.NOT_FOUND;
+    }
+
+    public Grade getGradeById(String id){
+         int index = getGradeIndex(id);
+         return index==Constants.NOT_FOUND ? new Grade() : getGrade(index);
+    }
+
+    public void submitGrade(Grade grade){
+        int index = getGradeIndex(grade.getId());
+            if(index == Constants.NOT_FOUND)   {
+                addGrade(grade);
+            }else{
+                updateGrade(index, grade);
+            }
+    }
 }
